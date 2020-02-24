bc9e0144e54a81a6d3b2759acc41c29cb8f5555a
yinm <yinm.cminor9@gmail.com>
2020-02-24 16:52:16 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const sourceMap = true;
+
+module.exports = {
+  mode: "development",
+
+  module: {
+    rules: [
+      {
+        test: /\.scss/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              url: false,
+              sourceMap,
+              importLoaders: 2
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap
+            }
+          }
+        ]
+      }
+    ]
+  },
+
+  devServer: {
+    open: true,
+    contentBase: "./public",
+    watchContentBase: true
+  }
+};
